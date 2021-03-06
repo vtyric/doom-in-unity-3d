278c9bf8583fefe1d3090684837f1ea7fb28bc42
KPEesti <iwantoseeyou1@gmail.com>
2021-03-07 01:14:43 +0500

    Дополнение к прошлому Commit

---
+++ Assets/Sqripts/Door.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject door;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.SetBool("OpenTriger", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        animator.SetBool("OpenTriger", false);
+    }
+
+}
