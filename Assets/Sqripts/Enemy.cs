7e643d4c96672462966256a1be4cccbf1b643977
qwqw ffe <misha.torno@gmail.com>
2021-03-08 23:33:44 +0500

    добавил движения врага на персонажа, 1 баг(выпадаю за карту)

--- Assets/Sqripts/Enemy.cs
+++ Assets/Sqripts/Enemy.cs
@@ -1,10 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float health=100;
+    [SerializeField] private float sightRange;
+    [SerializeField] private float walkPointRange;
+    [SerializeField] private LayerMask whatIsPlayer, whatIsGround;
+
+    private Transform player;
+    private NavMeshAgent enemy;
+    private Vector3 walkPoint;
+    private bool playerInSightRange;
+    private bool walkPointSet;
+
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+        enemy = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+
+        if (!playerInSightRange)
+            Patrolling();
+        else
+            ChasePlayer();
+    }
+
+    private void Patrolling()
+    {
+        if (!walkPointSet)
+            SearchWalkPoint();
+        else
+            enemy.SetDestination(walkPoint);
+
+        walkPointSet = (transform.position - walkPoint).magnitude >= 1f;
+    }
+
+    private void SearchWalkPoint()
+    {
+        var rndX = Random.Range(-walkPointRange, walkPointRange);
+        var rndZ = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(
+            transform.position.x + rndX,
+            transform.position.y,
+            transform.position.z + rndZ);
+
+        walkPointSet = Physics.Raycast(
+            walkPoint,
+            -transform.up,
+            2f,
+            whatIsGround);
+    }
+
+    private void ChasePlayer()
+    {
+        enemy.SetDestination(player.position);
+    }
 
     public void TakeDamage(float amount)
     {
@@ -18,4 +78,5 @@ public class Enemy : MonoBehaviour
     {
         Destroy(gameObject);
     }
+
 }
